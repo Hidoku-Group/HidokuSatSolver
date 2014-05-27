599634ff3c87e65500d74b1e17ffb9035f45480d
Michael Guenther <guenthermi50@yahoo.de>
2014-05-27 17:24:02 +0200

    start to implement parser for hidoku files

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int parseHidoku(char* filename) {
+	string line;
+	ifstream myfile(filename);
+
+	if (myfile.is_open()) {
+		//erste Zeile
+		getline(myfile,line);
+		int size;
+		sscanf (line.c_str(), "hidoku with size %d",&size);
+		cout << size;
+		vector<int> result;
+		int i=0, linecounter = 1;
+		while ( getline (myfile,line) )	{
+			linecounter++;
+			//nur die zeilen in denen zahlen kodiert sind
+			if(linecounter%4 != 0) {
+				continue;
+			}
+			
+			//zahlen extrahieren
+			for(int j=0;j<size;j++) {
+				
+	
+			}
+			result[i] = line;
+			cout << line << '\n';
+		}
+
+		myfile.close();
+	}
+
+}
+
+int main() {
+	parseHidoku("easy/hidoku-3-6-1.txt");
+	return 10;
+}
+
+
